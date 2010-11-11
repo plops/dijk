7e5b419b9908aebf780674b6a06fb670989efdbf
m <martin@uffz.(none)>
2010-11-11 08:12:57 +0000

    tried to solve the maze with dijkstra but doesn't seem to work

--- dijkstra.lisp
+++ dijkstra.lisp
@@ -48,20 +48,25 @@
 ;; potentially one could leave out one case if the input graph isn't
 ;; directed
 (defun dist-between (u v graph len)
-  "Try to find distance either in u or v entry of graph."
+  "Try to find distance either in u or v entry of graph. If LEN is nil
+distance defaults to unity."
   (let ((p (position v (elt graph u))))
     (if p
-	(elt (elt len u) p)
+	(if len
+	    (elt (elt len u) p)
+	    1)
 	(let ((p (position u (elt graph v))))
 	  (when p
-	    (elt (elt len v) p))))))
+	    (if len
+		(elt (elt len v) p)
+		1))))))
 #+nil
 (dist-between 0 2 graph len)
 
 #+nil
 (dist-between 2 0 graph len)
 
-(defun dijkstra (graph len &optional (source 0))
+(defun dijkstra (graph &key (len nil) (source 0))
   "Return an array of the length of the shortest routes from the
 source to the corresponding node."
   (let ((unvisited (loop for i below (length graph) collect i))
@@ -149,13 +154,47 @@ source to the corresponding node."
 ; b
 
 (defparameter m (read-pgm "maze.pgm"))
+(defparameter aa (make-array (list 50 50)
+			    :element-type '(unsigned-byte 8)))
+(defparameter bb (make-array (list 50 50)
+			    :element-type '(unsigned-byte 8)))
+(defparameter ll (make-array (list 50 50)
+			    :element-type '(unsigned-byte 8)))
+(defparameter rr (make-array (list 50 50)
+			    :element-type '(unsigned-byte 8)))
+(defun conv (a)
+  (if (= a 0)
+      0
+      1))
 (dotimes (j 50)
- (dotimes (i 50) ;; centers, border pixels are at 3,4,5
-   (let* ((x (+ 3 2 (* 8 j)))
-	  (y (+ 3 2 (* 8 i)))
-	  (a (/ (aref m (+ y 3) x) 256))
-	  (l (/ (aref m y (- x 3)) 256))
-	  (r (/ (aref m y (+ x 3)) 256))
-	  (b (/ (aref m (- y 3) x) 256)))
-     (setf (aref m y x) a)))
-(write-pgm "o.pgm" m)
+  (dotimes (i 50) ;; centers, border pixels are at 3,4,5
+    (let* ((x (+ 3 2 (* 8 j)))
+	   (y (+ 3 2 (* 8 i)))
+	   (a (conv (aref m (+ y 3) x)))
+	   (l (conv (aref m y (- x 3))))
+	   (r (conv (aref m y (+ x 3))))
+	   (b (conv (aref m (- y 3) x))))
+     (setf (aref aa j i) a
+	   (aref bb j i) b
+	   (aref ll j i) l
+	   (aref rr j i) r))))
+(defparameter graph (make-array (* 50 50) :element-type 'cons
+				:initial-element nil))
+(dotimes (j 49)
+  (dotimes (i 49)
+    (let ((p (+ (* j 50) i)))
+      (when (aref bb j i)
+	(push (+ p 50) (aref graph p)))
+      (when (aref rr j i)
+	(push (+ p 1) (aref graph p))))))  
+
+#+nil
+(defparameter *sol* (dijkstra graph))
+
+(dotimes (j 50)
+  (dotimes (i 50)
+    (let ((p (+ (* j 50) i)))
+      (setf (aref o j i) (floor (if (< (elt *sol* p) 255)
+				    (elt *sol* p)
+				    255))))))
+(write-pgm "o.pgm" o)
